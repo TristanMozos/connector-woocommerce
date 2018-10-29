253a0590db45b7786150d51bfd0da90394947968
Alvaro Marcos <alvaro.marcos@factorlibre.com>
2018-10-29 14:23:40 +0100

    Exportar inventarios

---
+++ connector_woocommerce/models/product_variant/exporter.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# © 2018 FactorLibre - Hugo Santos <hugo.santos@factorlibre.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo.addons.component.core import Component
+
+
+class ProductInventoryExporter(Component):
+    _name = 'woo.product.inventory.exporter'
+    _inherit = 'woocommerce.exporter'
+    _apply_on = ['woo.product.product']
+    _usage = 'product.inventory.exporter'
+
+    def _get_data(self, binding, fields):
+        result = {}
+        if 'woo_qty' in fields:
+            result.update({
+                'stock_quantity': binding.woo_qty,
+                'manage_stock': True,
+                'in_stock': binding.woo_qty > 0
+            })
+        return result
+
+    def run(self, binding, fields):
+        """ Export the product inventory to WooCommerce """
+        external_id = self.binder.to_external(binding)
+        template_ids = binding.odoo_id.product_tmpl_id.woocommerce_bind_ids
+        for template in template_ids:
+            template_id = template.external_id
+            break
+        if template_id:
+            data = self._get_data(binding, fields)
+            self.backend_adapter.write(external_id, template_id, data)
